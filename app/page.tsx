05ab0f4b8571eec7306262c66df4ba71d1449211
Nicksm98 <nicholas.sanmartin20@gmail.com>
2025-02-27 04:22:17 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,56 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button'
 
-export default function Home() {
+export default function Home () {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className='h-screen w-screen flex flex-col'>
+      <header className='h-16 flex items-center justify-between px-8 '>
+        <div className='flex items-center gap-4'>
+          <p className='font-bold text-lg tracking-wide'>SKINSTRIC</p>
+          <span className='text-sm tracking-widest'>[ INTRO ]</span>
         </div>
+        <Button className='bg-black text-white rounded-none hover:bg-gray-800'>
+          ENTER CODE
+        </Button>
+      </header>
+      <main className='h-[100%] flex flex-col items-center justify-center bg-white text-black relative'>
+        <h1 className='text-[88px] tracking-tighter leading-none font-300 text-center mb-12'>
+          Sophisticated <br /> skincare
+        </h1>
+        <div className='absolute left-[10%] top-1/2 transform -translate-y-1/2 flex items-center justify-center'>
+          <div className='w-14 h-14 border-[2px] border-black transform rotate-45 flex items-center justify-center'>
+          <button className='-rotate-45 text-lg font-bold'>
+              <div className='w-0 h-0 border-l-8 border-r-8 border-b-12 transform rotate-270 border-transparent border-b-black'></div>
+            </button>
+          </div>
+          <div className='ml-7 text-sm tracking-wide'>DISCOVER A.I.</div>
+        </div>
+        <div className='absolute right-[10%] top-1/2 transform -translate-y-1/2 flex items-center'>
+          <p className='mr-7 text-sm tracking-wide'>TAKE TEST</p>
+          <div className='w-14 h-14 border-[2px] border-black transform rotate-45 flex items-center justify-center'>
+          <button className='-rotate-45 text-lg font-bold'>
+              <div className='w-0 h-0 border-l-8 border-r-8 border-b-12 transform rotate-90 border-transparent border-b-black'></div>
+            </button>
+          </div>
+        </div>
+        <div className='flex flex-col items-center opacity-50'>
+          <div className='relative w-[80px] h-[80px] flex items-center justify-center opacity-30 pb-2'>
+            <div className='absolute w-full h-full transform rotate-45 border border-[#1A1B1C] bg-white z-10'></div>
+            <div className='absolute w-[70%] h-[70%] transform rotate-45 border border-[#1A1B1C] bg-white z-20'></div>
+            <div className='absolute w-2 h-2 bg-[#1A1B1C] transform rotate-45 pb-[] z-30'></div>
+            <div className='absolute w-[100px] h-[1px] bg-[#1A1B1C] transform rotate-45 z-0'></div>
+            <div className='absolute w-[100px] h-[1px] bg-[#1A1B1C] transform -rotate-45 z-0'></div>
+          </div>
+          <p className='mt-4 text-sm text-gray-600 text-center'>
+            TAP AND HOLD <br /> TO PROCEED TO SECTION
+          </p>
+        </div>
+        <p className='absolute bottom-8 left-8 text-[12px] text-[#1A1B1C] max-w-xl'>
+          SKINSTRIC DEVELOPED AN A.I. THAT CREATES 
+          <br />A HIGHLY-PERSONALISED ROUTINE TAILORED TO
+          <br /> WHAT YOUR SKIN NEEDS.
+        </p>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+  )
 }
+
