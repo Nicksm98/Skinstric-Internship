1d5c7ee1b136454b6fc48d163c77cec991aa9e78
Nicksm98 <nicholas.sanmartin20@gmail.com>
2025-03-10 03:40:13 -0400

    bug fixing

--- app/(routes)/analysis/aicamera/page.tsx
+++ app/(routes)/analysis/aicamera/page.tsx
@@ -231,7 +231,7 @@ const AiCamera = () => {
       <div className='left-btn absolute left-[32px] bottom-[40px] flex items-center justify-center z-10'>
         <div className='outer w-[34px] h-[34px] border-[2px] border-black transform rotate-45 flex items-center justify-center'>
           <div className='inner w-[34px] h-[34px] border-dotted border-[2px] border-black flex items-center justify-center'>
-            <Link href='/Introduction/photo'>
+            <Link href='/introduction/photo'>
               <Button className='left-btn bg-transparent hover:bg-transparent -rotate-45'>
                 <div className='w-0 h-0 border-l-5 border-r-5 border-b-8 transform rotate-270 border-transparent border-b-black'></div>
               </Button>
